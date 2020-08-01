f13946ba55ce9a4439b0e2af65a2c6e36b88650f
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2020-08-01 16:23:02 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//this sets the background colour to be dark grey
+Color darkGreyColor = new Color(0XFF212128);
+
+/*class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0, // in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.deepOrangeAccent),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        // <Widget> is the type of items in the list.
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null disables the button
+          ),
+          // Expanded expands its child to fill the available space.
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}*/
+
 //stateless widgets don't allow for user interaction
 class MyScaffold extends StatefulWidget {
   @override
@@ -29,6 +69,77 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
 
+//this class builds a custom widget that prints the sliver list to the app body
+class CustomWidget extends StatelessWidget {
+  CustomWidget(this._index) {
+    debugPrint('initialize: $_index');
+  }
+
+  final int _index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          color: (_index % 2 != 0) ? Colors.white : Colors.deepOrangeAccent,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    textDirection: TextDirection.ltr,
+                    children: <Widget>[
+                      Text(
+                        'Date: ',
+                        textAlign: TextAlign.right,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Name: ',
+                        textAlign: TextAlign.right,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Distance: ',
+                        textAlign: TextAlign.right,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Icon(Icons.lightbulb_outline, size: 50.0),
+                ],
+              ),
+            ),
+          ],
+        )
+    );
+  }
+}
+
 class MyScaffoldState extends State<MyScaffold>{
   int selectedIndex = 1; //the selected index keeps track of the icon that has been selected in the navigation bar, setting the index to 1 automatically sets the default selected icon to the middle icon
 
@@ -39,27 +150,6 @@ class MyScaffoldState extends State<MyScaffold>{
     });
   }
 
-  //currently the list is not being displayed
-  final List<String> entries = <String>['A', 'B', 'C'];
-  Widget buildList(){
-    return ListView.separated(
-      padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 1.0),
-      itemCount: entries.length,
-      itemBuilder: (BuildContext context, int index){
-        return Container(
-          height: 80,
-          decoration: new BoxDecoration(
-            borderRadius:
-            BorderRadius.all(Radius.circular(10)),
-            color: Colors.lightBlueAccent[100],
-          ),
-          child: Center(child: Text('Entry ${entries[index]}')), //right now it
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-    );
-  }
-
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
@@ -67,30 +157,26 @@ class MyScaffoldState extends State<MyScaffold>{
       body: CustomScrollView(
         physics:  ClampingScrollPhysics(),
         slivers: <Widget>[
-          buildHeader(screenHeight),
-          buildBody(screenHeight),
+          buildHeader(screenHeight), //displays the header extension that holds additional info (looks like the extension of the app bar)
+          //buildBody(screenHeight), //prints the body, might be deleted
           SliverList( //SilverList is used for CustomAppBar, unlike ListView it has more capabilities
             delegate: SliverChildListDelegate(
-                [
-                  Container(color: Colors.red, height: 150.0),
-                  Container(color: Colors.purple, height: 150.0),
-                  Container(color: Colors.green, height: 150.0),
-                ]
+                List.generate(10, (index) => CustomWidget(index)).toList() //calls the CustomWidget class and prints the
             ),
           )
         ],
       ),
 
       //this tog displays the bottom navigation bar
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar( //add the onPressed function to declare the action that needs to be done when the icons in the navigation bar has been clicked (make a function to declare for onPressed)
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.business),
             title: Text('Business'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.school),
@@ -145,90 +231,6 @@ class MyScaffoldState extends State<MyScaffold>{
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  SliverToBoxAdapter buildBody(double screenHeight){
-    return SliverToBoxAdapter(
-      child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 10.0), //moves the container lower as vertical value goes up
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-          decoration: BoxDecoration(
-            color: Colors.deepOrangeAccent,
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          'Date: ',
-                          textAlign: TextAlign.left,
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          'Name: ',
-                          textAlign: TextAlign.left,
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          'Distance: ',
-                          textAlign: TextAlign.left,
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Icon(Icons.lightbulb_outline, size: 50.0),
-                  ],
-                ),
-              ),
-              Container(
-                height: 100,
-                child: const  Text(
-                  'Entry B',
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Container(
-                height: 100,
-                child: const Text(
-                  'Entry C',
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          )
       ),
     );
   }
